da2ee7222dde5a71f80e2b0a49edb436ddee82c4
Anon <anon@ubuntu.none>
2016-10-17 18:16:00 -0600

    adding live data

--- tigerr/tigerr.py
+++ tigerr/tigerr.py
@@ -4,10 +4,15 @@ kivy.require('1.9.2')
 
 from random import sample
 from string import ascii_lowercase
+import subprocess
+import json
 import webbrowser  # TODO: click on review row to open in browser
 
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.config import Config
+from kivy.properties import NumericProperty
+from kivy.properties import StringProperty
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.settings import SettingsWithSidebar
 
@@ -15,14 +20,30 @@ from settings import settings_json
 
 
 class Tigerr(BoxLayout):
-
     def populate(self):
-        # TODO: get from gerrit (ssh u@d.com -p 123 gerrit query)       
-        self.rv.data = [{'value': ''.join(sample(ascii_lowercase, 6))}
-                        for x in range(50)]
+        p = Config.get('gerrit', 'port')
+        u = Config.get('gerrit', 'user')
+        h = Config.get('gerrit', 'host')
+        _l = 10 
+        query = 'ssh -p {p} {u}@{h} gerrit query --format=JSON {q} limit:{l}'
+        _sq = 'status:open project:openstack/neutron'
+        proc = subprocess.Popen(query.format(p=p,u=u,h=h,q=_sq,_l=10).split(),
+                                stdout=subprocess.PIPE)
+        x, e = proc.communicate()
+        results = []
+        stats = []
+        for rr in x.split('\n'):
+            if rr:
+                _r = json.loads(rr)
+                if 'type' in _r:
+                    if _r['type'] == 'stats':
+                        stats = _r
+                else:
+                    results.append(_r)
+        self.rv.data = results
 
     def sort(self):
-        self.rv.data = sorted(self.rv.data, key=lambda x: x['value'])
+        self.rv.data = sorted(self.rv.data, key=lambda x: x[''])
 
     def clear(self):
         self.rv.data = []
@@ -41,6 +62,7 @@ class Tigerr(BoxLayout):
 
 
 class TigerrApp(App):
+
     def build(self):
         self.settings_cls = SettingsWithSidebar
         self.use_kivy_settings = False
@@ -50,16 +72,16 @@ class TigerrApp(App):
         config.setdefaults('gerrit', {
                 'host': 'review.openstack.org',
                 'port': 29418,
-                'user': 'Jdoe',
-                '': True})
+                'user': 'Jdoe'})
 
     def build_settings(self, settings):
-        settings.add_json_panel('Tigerr',
+        settings.add_json_panel('gerrit',
                                 self.config,
                                 data=settings_json)
 
     def on_config_change(self, config, section, key, value):
         print((config, section, key, value))
+
 
 if __name__ == '__main__':
     TigerrApp().run()
